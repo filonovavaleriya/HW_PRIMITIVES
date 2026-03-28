05d23a69c182d5997d3b2d102443b28a62e9e3af
Filonova Valeriya <filonova_valeriya@bk.ru>
2026-03-28 21:59:49 +0300

    Answer for the first task

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Объявляем переменную для хранения стоимости билета (в рублях)
+        int ticketPrice = 5000;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Объявляем переменную для количества рублей, необходимых для одной бонусной мили
+        int amountOneMiles = 20;
+
+        // Рассчитываем количество бонусных миль
+        // Дробные мили не начисляются — используем целочисленное деление
+        int bonusMiles = ticketPrice / amountOneMiles;
+
+        // Выводим результат на экран
+        System.out.println("Стоимость билета:" + ticketPrice + "рублей");
+        System.out.println("Количество начисленных миль:" + bonusMiles);
     }
 }
